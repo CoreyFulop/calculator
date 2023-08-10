0ee39af9592f95ffa96e2266517ad7a2a1a77b2a
Corey Fulop <corey.fulop@gmail.com>
2023-08-10 17:34:03 +0100

    Create function that updates display and display value variable
    Add event listeners to each number. Number is appended to the end of the display value

--- js/javascript.js
+++ js/javascript.js
@@ -23,3 +23,24 @@ let numberTwo = 0;
 function operate(a, b, operation) {
     return operation(a, b);
 }
+
+let displayValue = 0;
+
+let display = document.querySelector(".display");
+
+// const numberButtons = document.querySelectorAll(".number");
+// numberButtons.forEach(function (button) {
+//     button.addEventListener("click", updateDisplayValue);
+// });
+
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach(button => button.addEventListener("click", updateDisplayValue));
+
+function updateDisplayValue(e) {
+    let targetValue = e.target.id;
+    displayValue += targetValue;
+    if (displayValue[0] == "0") {
+        displayValue = displayValue.slice(1);
+    }
+    display.textContent = displayValue;
+}
